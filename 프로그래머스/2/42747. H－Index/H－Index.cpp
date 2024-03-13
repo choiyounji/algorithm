f68318dae4356a6eb1168d492936e6a7e83be45e
choiyounji <100260416+choiyounji@users.noreply.github.com>
2024-03-13 16:19:17 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -6,13 +6,15 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    sort(citations.begin(), citations.end(),greater<>());
+    sort(citations.begin(), citations.end());
     int p=1;
     int answer=0;
-    for(int i=0;i<citations.size();i++){
-        if(citations[i]>=i+1){
-            answer=i+1;
+    for(int i=citations.size()-1;i>=0;i--){
+        if(citations[i]>=p){
+            answer=p;
+            //break; 넣으면 안됨 왜;?
         }
+        p++;
     }
     return answer;
 }
