fc858e7e0e74074e202b5c8e04fc1eae67c5cd03
choiyounji <100260416+choiyounji@users.noreply.github.com>
2024-04-23 17:27:09 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct country {
+	int num;
+	int g, s, b;
+};
+vector<country> v;
+bool cmp(country f, country l) {
+	if (f.g == l.g) {
+		if (f.s == l.s) {
+			if (f.b == l.b) {
+				return f.num < l.num;
+			}
+			return f.b > l.b;
+		}
+		return f.s > l.s;
+	}
+	return f.g > l.g;
+}
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n, k;
+	cin >> n >> k;
+	v.assign(n, {0,0,0});
+	int q,w,e,r;
+	for (int i = 0; i < n; i++) {
+		cin >> q >> w >> e >> r;
+		v[q - 1].num = q;
+		v[q - 1].g = w;
+		v[q - 1].s = e;
+		v[q - 1].b = r;
+	}
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = n - 1; i >= 0; i--) {
+		if (i == k - 1) {
+			int cg, cb, cs;
+			cg = v[i].g;
+			cs = v[i].s;
+			cb = v[i].b;
+			int cnt = 0;
+			for (int j = i-1; j >= 0; j--) {
+				if (cg == v[j].g && cs == v[j].s && cb == v[j].b) {
+					cnt++;
+				}
+				else {
+					break;
+				}
+			}
+			cout << i - cnt + 1;
+
+		}
+	}
+	return 0;
+}
