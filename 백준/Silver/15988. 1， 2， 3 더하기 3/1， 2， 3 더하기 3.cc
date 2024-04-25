83b366d0d230a94cf1eddb83cb9fc04a1b4c42d0
choiyounji <100260416+choiyounji@users.noreply.github.com>
2024-04-25 16:15:21 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 16 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+long long dp[1000001] = { 0, };
+int main() {
+	int t;
+	cin >> t;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	for (int i = 4; i <= 1000000; i++) {
+		dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1])%1000000009;
+	}
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << dp[n] << "\n";
+	}
+	return 0;
+}
